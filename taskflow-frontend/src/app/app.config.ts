a87204af86553f11e3feb61e0d9b7f0a9f4bc281
Alexander <alexx.16@protonmail.com>
2025-10-08 06:46:54 -0500

    refactor: update Apollo configuration and imports in app.config.ts

--- taskflow-frontend/src/app/app.config.ts
+++ taskflow-frontend/src/app/app.config.ts
@@ -1,23 +1,14 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection, inject } from '@angular/core';
 import { provideRouter } from '@angular/router';
 //importaciones para trabajar comunicación con HTTP y graphql con Apollo
-import { provideHttpClient } from '@angular/common/http';
-import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
-// Corregir la importación de APOLLO_OPTIONS
-import { APOLLO_OPTIONS } from 'apollo-angular';
+import { provideHttpClient, HttpClient } from '@angular/common/http';
+import { InMemoryCache } from '@apollo/client/core';
+import { provideApollo } from 'apollo-angular';
 import { HttpLink } from 'apollo-angular/http';
 import { routes } from './app.routes';
 
 //URI del servidor GraphQL de Laravel
 const uri = 'http://127.0.0.1:8000/graphql';
-
-//Función para crear la configuración de Apollo
-export function createApollo(HttpLink: HttpLink): ApolloClientOptions<any> {
-  return {
-    link: HttpLink.create({ uri }),
-    cache: new InMemoryCache(),
-  }
-}
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -27,11 +18,14 @@ export const appConfig: ApplicationConfig = {
 
     //Configuraciones para trabajar con HTTP y Apollo
     provideHttpClient(),
-
-    {
-      provide: APOLLO_OPTIONS,
-      useFactory: createApollo,
-      deps: [HttpLink]
-    }
+    
+    provideApollo(() => {
+      const httpClient = inject(HttpClient);
+      const httpLink = new HttpLink(httpClient);
+      return {
+        link: httpLink.create({ uri }),
+        cache: new InMemoryCache(),
+      };
+    })
   ]
 };
