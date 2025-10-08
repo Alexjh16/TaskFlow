0a07cc78209a694cda14a4e24af8051ed8bd22ee
Alexander <alexx.16@protonmail.com>
2025-10-07 23:38:31 -0500

    Schemas para Category y Task, rutas de pruebas.

--- taskflow-backend/routes/web.php
+++ taskflow-backend/routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Task;
+use App\Models\Category;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('task', function(){
+    return count(Task::all());
+});
+
+Route::get('categories', function(){
+    return count(Category::all());
 });
